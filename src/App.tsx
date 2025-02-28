ea0b7b0e329d5afff0d6adb4c62f3e9d0e0298be
austzdee <167780868+austzdee@users.noreply.github.com>
2025-02-28 23:15:30 +0000

    React Project

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+import ListGroup from "./assets/components/ListGroup";
+
+function App(){
+    return <div><ListGroup /></div>
+}
+
+export default App;
---
+++ src/Message.tsx
@@ -0,0 +1,10 @@
+//Pascal casing
+
+function Message() {
+        //JSX: Javascript XML code:
+        const name = 'Danito';
+        return <h1>Hello {name} world</h1>;
+        return <h1>Good morning my frinds</h1>
+}
+
+export default Message;
