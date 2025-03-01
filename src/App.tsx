d50c2bd3f5a0979a6852cc773bb47787b4633a5d
austzdee <167780868+austzdee@users.noreply.github.com>
2025-03-01 22:52:46 +0000

    learning state,props

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,19 @@
 import ListGroup from "./assets/components/ListGroup";
 
 function App(){
-    return <div><ListGroup /></div>
+
+    let items = [
+        "New York",
+        "Akure",
+        "San Francisco",
+        "Manchester",
+        "London",
+        "Lagos",
+        "Wigan",
+        "Abuja",
+      ];
+
+    return <div><ListGroup  items={items}  heading= "Cities" /></div>
 }
 
 export default App;
